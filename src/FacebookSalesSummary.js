5931277d90ee5b5c31adca1def4d16e17b1ae7b5
typekjezeqcz <110474183+typekjezeqcz@users.noreply.github.com>
2023-12-12 10:55:42 +0100

    Update FacebookSalesSummary.js

--- src/FacebookSalesSummary.js
+++ src/FacebookSalesSummary.js
@@ -14,6 +14,14 @@ function SummaryCard({ title, value }) {
   );
 }
 
+function formatCurrency(value) {
+  return new Intl.NumberFormat('en-US', {
+      style: 'currency',
+      currency: 'USD',
+  }).format(value);
+}
+
+
 // Component to calculate and display summary cards
 const SummaryCardsContainer = ({ adaccountsData }) => {
   // Assuming adaccountsData is an array of objects with revenue, order_count, and roas properties
@@ -24,10 +32,10 @@ const SummaryCardsContainer = ({ adaccountsData }) => {
 
   return (
     <div className="mt-5 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4 max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 ">
-      <SummaryCard title="Facebook Total Sales" value={`$${totalSales.toFixed(2)}`} />
+      <SummaryCard title="Facebook Total Sales" value={formatCurrency(totalSales)} />
       <SummaryCard title="Facebook Orders Count" value={totalOrders} />
       <SummaryCard title="Facebook Average ROAS" value={`${averageROAS.toFixed(2)}`} />
-      <SummaryCard title="Facebook Total Spend" value={`$${totalSpend.toFixed(2)}`} />
+      <SummaryCard title="Facebook Total Spend" value={formatCurrency(totalSpend)} />
     </div>
 
   );
