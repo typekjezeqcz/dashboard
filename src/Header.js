9ea753056eda5a8889a31abb2d799b2353b24b6c
typekjezeqcz <110474183+typekjezeqcz@users.noreply.github.com>
2023-12-13 19:38:23 +0100

    Update Header.js

--- src/Header.js
+++ src/Header.js
@@ -1,7 +1,6 @@
 import React from 'react';
-import { useNavigate } from 'react-router-dom';
-import { Link } from 'react-router-dom';
-import logo from './10fc black.png'
+import { useNavigate, Link } from 'react-router-dom';
+import logo from './10fc black.png';
 
 const Header = () => {
   const navigate = useNavigate();
@@ -14,24 +13,29 @@ const Header = () => {
 
   return (
     <header className="bg-white py-4">
-      <nav className="max-w-6xl mx-auto px-6 flex justify-between items-center">
-        <div className="flex items-start space-x-6">
-        <Link to="/" className="text-gray-900 hover:text-blue-600 font-medium">
-            <img src={logo} alt="Dashboard Logo" style={{ height: '25px' }} /> {/* Adjust size as needed */}
+      <nav className="max-w-6xl mx-auto px-6 flex flex-col md:flex-row justify-between items-center">
+        {/* Logo */}
+        <div className="mb-4 md:mb-0">
+          <Link to="/" className="text-gray-900 hover:text-blue-600 font-medium">
+            <img src={logo} alt="Dashboard Logo" className="h-6 mx-auto md:mx-0" />
           </Link>
-           {userRole === 'admin' && (
+        </div>
+
+        {/* Links and Logout Button */}
+        <div className="flex flex-row justify-between items-center space-x-6 w-full md:w-auto">
+          {userRole === 'admin' && (
             <>
               <Link to="/users" className="text-gray-900 hover:text-blue-600 font-medium">Create User</Link>
               <Link to="/admin-dashboard" className="text-gray-900 hover:text-blue-600 font-medium">Manage Users</Link>
             </>
           )}
+          <button
+            onClick={handleLogout}
+            className="text-blue-600 hover:text-white border border-blue-600 hover:bg-blue-600 rounded-full px-5 py-2 transition-colors duration-300"
+          >
+            Log Out
+          </button>
         </div>
-        <button
-          onClick={handleLogout}
-          className="text-blue-600 hover:text-white border border-blue-600 hover:bg-blue-600 rounded-full px-5 py-2 transition-colors duration-300"
-        >
-          Log Out
-        </button>
       </nav>
     </header>
   );
