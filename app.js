5f79d11e0ab479dd92209c629d963c9311212766
typekjezeqcz <110474183+typekjezeqcz@users.noreply.github.com>
2024-01-02 22:29:45 +0100

    Update app.js

--- app.js
+++ app.js
@@ -19,13 +19,13 @@ const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server, {
   cors: {
-      origin: "http://localhost:1500",  // Allow your client origin
+      origin: "http://roasbooster.com",  // Change to your deployed site's address
       methods: ["GET", "POST"]  // Allow only necessary methods
   }
 });
 
 app.use(cors({
-  origin: "http://localhost:1500",  // Allow your client origin
+  origin: "http://roasbooster.com",  // Change to your deployed site's address
   methods: ["GET", "POST"]  // Allow only necessary methods
 }));
 
